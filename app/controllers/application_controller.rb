d765641ac63e0f13acc8c99a903b9f1473ebcb9e
Anuj Panchal <anujpan1@gmail.com>
2019-01-29 17:05:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,14 +13,4 @@ class ApplicationController < Sinatra::Base
     erb :layout
   end
 
-  helpers do
-    def self.current_user(session)
-      User.find_by_id(session["user_id"])
-    end
-  
-     def self.logged_in(session)
-      !!session["user_id"]
-    end
-  end
-
 end
