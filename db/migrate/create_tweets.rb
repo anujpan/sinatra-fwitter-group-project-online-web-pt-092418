4e3c94a3ea43682dd59e576625f358d4bd378c39
Anuj Panchal <anujpan1@gmail.com>
2019-01-29 16:51:34 +0000

    Automatically backed up by Learn

--- db/migrate/create_tweets.rb
+++ db/migrate/create_tweets.rb
@@ -1,7 +1,7 @@
 class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
-      t.string :username
+      t.string :content
       t.string :email
       t.string :password_digest
     end
